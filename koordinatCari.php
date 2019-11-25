0cb73aaec31bdefa6f78454f94866a475712c0aa
nonnywind <42970258+nonnywind@users.noreply.github.com>
2019-11-25 19:43:59 +0700

    update bug

--- koordinatCari.php
+++ koordinatCari.php
@@ -1,6 +1,6 @@
 <?php
 include 'functions/functionsMaps.php';
-$konfigurasi = new konfigurasi();
+$konfigurasi = new konfigurasi();aaa
 $getData = $konfigurasi->tampilDataPencarianLokasi('tabel_data','artikel_id',$_GET['artikel_id']);
 header("Content-type: text/xml");
 try {
